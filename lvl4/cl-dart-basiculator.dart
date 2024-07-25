1f5e87a0c0dc55fe6b54e5c2b905eb9139c181c8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-25 13:06:24 +0530

    feat: Add cl-dart-basiculator

---
+++ lvl4/cl-dart-basiculator.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+void main() {
+  print("BasiCulator: The Basic Calculator");
+
+  while(true) {
+    print('''\n1) Addition
+2) Subtraction
+3) Multiplication
+4) Division''');
+    stdout.write("Enter the operation to perform (1-4): ");
+    int choice = int.parse(stdin.readLineSync() ?? '');
+
+    stdout.write("Enter the first number: ");
+    double number1 = double.parse(stdin.readLineSync() ?? '');
+
+    stdout.write("Enter the second number: ");
+    double number2 = double.parse(stdin.readLineSync() ?? '');
+
+    switch(choice) {
+      case 1:
+        performAddition(number1, number2);
+        break;
+
+      case 2:
+        performSubtraction(number1, number2);
+        break;
+
+      case 3:
+        performMultiplication(number1, number2);
+        break;
+
+      case 4:
+        performDivision(number1, number2);
+        break;
+    }
+  }
+}
+
+void performAddition(double a, double b) {
+  print("The sum of $a and $b is ${a+b}");
+}
+
+void performSubtraction(double a, double b) {
+  print("The differene between $a and $b is ${a-b}");
+}
+
+void performMultiplication(double a, double b) {
+  print("The product of $a and $b is ${a*b}");
+}
+
+void performDivision(double a, double b) {
+  if (b == 0) {
+    print("Division by zero is not possible!");
+  } else {
+    print("The quotient of $a and $b is ${a/b}");
+  }
+}
